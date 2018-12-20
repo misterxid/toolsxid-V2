2619bd0d3ee2351e63f6a1a73aa166de35262135
Mr_XiD <44462886+safri1234@users.noreply.github.com>
2018-12-20 22:02:42 +0700

    Create installtool.sh

---
+++ installtool.sh
@@ -0,0 +1,192 @@
+#LU kira ni tools gua reqode punya orang wkwkwkw
+#makanya belajar dulu
+#reqode silahkan
+clear
+#Variables
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+
+toilet -f standard " MR_XiD " -F gay
+echo "       ***************************************** " | 
+lolcat
+echo "       *           ~D35TR0Y SQUAD~            * " | 
+lolcat
+echo "       * Author   : MR_XiD                  * " | 
+lolcat
+echo "       * No Whatsapp : 083851312460            * " | 
+lolcat
+echo "       * facebook : susu bendera            * " | 
+lolcat
+echo "       * Thanks To : ALL member D35TR0Y SQUAD* " | 
+lolcat
+echo "       * SPECIAL TO        * " | 
+lolcat
+echo "       * MA || CYB3R GHOST || CRUSHER ||MR EXPLOR5 " | 
+lolcat
+echo "       *||  ALL MEMBER TEAM      * " | 
+lolcat
+echo "       ***************************************** " | 
+lolcat
+
+
+sleep 1
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+clear
+sleep 1
+exit
+}
+
+echo ""
+echo ""
+echo $red.     " ################################## " 
+echo $white     " #  AYO S I L A H K A N  D I P I L I H # " 
+echo $white    " ################################## " 
+echo ""
+echo  $green "1. Install Ubuntu" 
+echo "========================================" | lolcat
+echo  $cyan  "2. Install Katolin" 
+echo "========================================" | lolcat
+echo  $blue  "3. Install santet online" 
+echo "========================================" | lolcat
+echo  $green "4. Install Admin-Finder" 
+echo "========================================" | lolcat
+echo  $cyan  "5. Install D-TECT" 
+echo "========================================" | lolcat
+echo  $blue  "6. Install Tuan Badut" 
+echo "========================================" | lolcat
+echo  $red  "18+. Keluar" 
+echo "========================================" | lolcat
+echo $green
+read -p " root@MR_XiD:~#" pil;
+
+case $pil in
+1) clear
+toilet -f standard " MR_XiD " -F gay
+apt update && apt upgrade
+clear
+apt install git
+clear
+apt install wget
+clear
+apt install proot
+clear
+git clone https://github.com/Neo-Oli/termux-ubuntu.git
+clear
+cd ~/termux-ubuntu
+clear
+chmod +x ubuntu.sh
+clear
+sh ubuntu.sh
+;;
+2) clear
+toilet -f standard "  MR_XiD  " -F gay
+pkg update
+clear
+pkg upgrade
+clear
+pkg install git
+clear
+pkg install python2
+clear
+pkg install nano
+clear
+pkg install gnupg
+clear
+git clone https://github.com/LionSec/katoolin.git
+clear
+ls
+clear
+cd katoolin
+clear
+ls
+clear
+python2 katoolin.py
+nano katoolin.py.
+;;
+3) clear
+toilet -f standard " MR_XiD " -F gay
+ apt update && apt upgrade 
+clear
+  apt install python2 
+clear
+  git clone https://github.com/Gameye98/santet-online 
+clear
+ cd santet-online 
+clear
+ python2 santet.py
+ ;;
+4) clear
+toilet -f standard " MR_XiD " -F gay
+apt update && apt upgrade
+clear
+pkg install python2
+clear
+pkg install git
+clear
+git clone https://github.com/AdheBolo/AdminFinder
+clear
+ls
+clear
+cd AdminFinder
+clear
+chmod 777 AdminFinder.py
+clear
+python2 AdminFinder.py
+;;
+5) clear
+toilet -f standard " MR_XiD " -F gay
+git clone https://github.com/shawarkhanethicalhacker/D-TECT
+cd D-TECT
+clear
+chmod +x d-tect.py
+clear
+python2 d-tect.py
+;;
+6) clear
+toilet -f standard " MR_XiD " -F gay
+apt update && apt upgrade
+clear
+pkg install python python2 vim figlet curl
+clear
+pkg install php
+clear
+pip2 install lolcat
+clear
+pkg install git
+clear
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv1
+clear
+cd TOOLSINSTALLERv1
+clear
+chmod +x Tuanb4dut.sh
+clear
+sh Tuanb4dut.sh
+;;
+99) echo "[D35TROY SQUAD]" | lolcat
+echo "ORANG NEWBYE WAJARIN" | lolcat
+echo "W Gans Tq ;v" | lolcat
+echo 'G' | lolcat
+echo 'O' | lolcat
+echo 'O' | lolcat
+echo 'D' | lolcat
+echo 'B' | lolcat
+echo 'Y' | lolcat
+echo 'E' | lolcat
+echo 'No Sistem Is Safe......' | lolcat
+exit
+;;
+
+esac
+done
+done
